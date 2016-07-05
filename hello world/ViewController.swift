19b71360648ce05f1c1659b7f40d90e1f0efcd63
山口仁崇 <falcon@yamaguchihitoshitakashi-no-MacBook-Pro.local>
2016-07-05 11:32:46 +0900

    Initial Commit

---
+++ hello world/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hello world
+//
+//  Created by 山口仁崇 on 2016/07/05.
+//  Copyright (c) 2016年 Masataka Yamaguchi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
